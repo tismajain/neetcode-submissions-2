b5cfd0c20a09e9982e2cd9b75d31dda52179a73d
Tisma Jain <159927171+tismajain@users.noreply.github.com>
2026-09-19 23:56:41 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    int robRange(int[] nums, int start, int end) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int curr = Math.max(prev1, prev2 + nums[i]);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+
+    public int rob(int[] nums) {
+        if (nums.length == 1)
+            return nums[0];
+
+        return Math.max(
+            robRange(nums, 0, nums.length - 2),
+            robRange(nums, 1, nums.length - 1)
+        );
+    }
+}
