d2497715f336cbc62778e15428baef59d92402ee
Tisma Jain <159927171+tismajain@users.noreply.github.com>
2026-09-19 17:22:16 +0530

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans=new int[temperatures.length];
+        Stack<Integer> st=new Stack<>();
+        
+        for(int i=0;i<temperatures.length;i++)
+        {
+            while(!st.isEmpty() && temperatures[st.peek()]<temperatures[i])
+            {
+                int val=st.pop();
+                ans[val]=i-val;
+            }
+            st.push(i);
+        }
+        return ans;
+
+    }
+}
