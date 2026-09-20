31a44636c8856b2900e78167e7798e8a3f59e315
Tisma Jain <159927171+tismajain@users.noreply.github.com>
2026-09-20 16:05:50 +0530

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.java
@@ -0,0 +1,56 @@
+class Solution {
+    boolean dfs(List<List<Integer>> graph, int[] state, List<Integer> ans,int idx)
+    {
+        if(state[idx]==2)
+        {
+            return false;
+        }
+        if(state[idx]==1)
+        {
+            return true;
+        }
+        state[idx]=1;
+        for(int neigh: graph.get(idx))
+        {
+            
+                if(dfs(graph,state, ans, neigh))
+                {
+                    return true;
+                }
+            
+        }
+        state[idx]=2;
+        ans.add(idx);
+        return false;
+
+    }
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        
+        List<List<Integer>> graph=new ArrayList<>();
+        for(int i=0;i<numCourses;i++)
+        {
+            graph.add(new ArrayList<>());
+        }
+        for(int[] c:prerequisites)
+        {
+            graph.get(c[1]).add(c[0]);
+        }
+        int[] state=new int[numCourses];
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<numCourses;i++)
+        {
+            if(state[i]==0 && dfs(graph,state,ans,i))
+            {
+                return new int[0];
+            }
+        }
+        Collections.reverse(ans);
+        int[] res=new int[numCourses];
+        for(int i=0;i<numCourses;i++)
+        {
+            res[i]=ans.get(i);
+        }
+        return res;
+
+    }
+}
