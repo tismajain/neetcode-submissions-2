c76e7ee12554d48468f966c7edf27334c5550f55
Tisma Jain <159927171+tismajain@users.noreply.github.com>
2026-09-20 13:44:57 +0530

    Add: partition-labels - submission-2

---
+++ Data Structures & Algorithms/partition-labels/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> ans=new ArrayList<>();
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            map.put(s.charAt(i),i);
+        }
+        int left=0,right=0;
+        
+        int max=map.get(s.charAt(0));
+        while( right<s.length())
+        {
+            max=Math.max(max,map.get(s.charAt(right)));
+            if(right==max)
+            {
+                ans.add(right-left+1);
+                left=right+1;
+                right++;
+                
+                
+                continue;
+            }
+            
+            right++;
+           
+
+        }
+        return ans;
+    }
+}
