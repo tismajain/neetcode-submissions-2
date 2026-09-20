e0dfc084aa4efe9e7abfc8c646646749a1b24f39
Tisma Jain <159927171+tismajain@users.noreply.github.com>
2026-09-20 16:13:49 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set=new HashSet<>();
+        for(String str:wordDict)
+        {
+            set.add(str);
+        }
+        boolean[] dp=new boolean[s.length()+1];
+        dp[0]=true;
+        for(int i=0;i<=s.length();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(dp[j] && set.contains(s.substring(j,i)))
+                {
+                    dp[i]=true;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
